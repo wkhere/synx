54de297a037e0fabdcd5385b423dbd9613c7be4e
Wojciech Kaczmarek <wojtekk@kofeina.net>
2022-01-14 16:41:10 +0100

    doc

--- synx.go
+++ synx.go
@@ -2,6 +2,15 @@ package synx
 
 import "sync"
 
+// MarkFirst is a combination of WaitGroup and Once,
+// allowing to synchronize all workers and mark
+// occurence of some condition in one of them,
+// like, the error.
+// It is similar to but different from x/sync/errgroup,
+// which gives back only the first error occured.
+// In the case one would want to do something with all the errors
+// separately and be interested in an extra mark that any ever happened,
+// this is the way to go.
 type MarkFirst struct {
 	occured bool
 	once    sync.Once
